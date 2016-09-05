34b0246621ff68873f37a8b6ac9ed51e72dec916
clarmso <clare.m.so@gmail.com>
2016-09-05 17:55:40 -0400

    Add "Where Am I" to Portfolio

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -6,7 +6,7 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+var pkg = require('../package.json');
 
 // Set the banner content
 var banner = ['/*!\n',
