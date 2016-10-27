ee1da886fdc954531ec151a39dca3d77f63193c9
clarmso <clare.m.so@gmail.com>
2016-10-26 22:46:28 -0400

    Remove extraneous files

--- client/js/contact_me.js
+++ client/js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("#contactForm input,#contactForm textarea, #g-recaptcha").jqBootstrapValidation({
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
