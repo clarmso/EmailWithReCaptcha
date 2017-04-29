a017a29cf0ce74f655342b8420c34850ba58213a
clarmso <clare.m.so@gmail.com>
2017-04-29 00:38:32 -0400

    Enable CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-//Initialize the Express library, used to create a web server
+// *** Express Server Setup ***
+
 var express = require("express");
 var app = express();
 
@@ -6,63 +7,86 @@ var app = express();
 var compression = require("compression");
 app.use(compression());
 
-// Set up mailgun to send email. body-parser is required for the json format.
-var mailgun = require('mailgun-js') ({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOMAIN
-} );
-
 // body-parser for the json
 var bodyParser = require( 'body-parser' );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ))
 
-// Set up reCAPTCHA to prevent abuse of the mail form
-var recaptcha = require('express-recaptcha');
-recaptcha.init( process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY );
 
-// Security
-// Do not use SSL in the localhost because there's no certificate there.
+// *** Security ***
 
 var express_enforces_ssl = require('express-enforces-ssl');
-
-//var helmet = require('helmet');
-//app.use(helmet());
-//app.use(helmet.referrerPolicy());
+// Do not use SSL in the localhost because there's no certificate there.
 if ( process.env.NODE_ENV ) {
   app.enable('trust proxy');
   app.use(express_enforces_ssl());
 }
 
+var helmet = require('helmet');
+app.use(helmet());
+app.use(helmet.referrerPolicy());
+
+var cors = require('cors');
+var whitelist = [
+    process.env.ALLOWED_ORIGIN,
+];
+var corsOptions = {
+    origin: function(origin, callback){
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        console.log("Origin not allowed by CORS:" + origin);
+        callback(new Error('Origin not allowed by CORS.  The origin is ' + origin), false);
+      }
+    },
+    methods: "POST"
+};
+app.use(cors(corsOptions));
+
+
+// *** Mailgun ***
+
+var mailgun = require('mailgun-js') ({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
+} );
+
+
+// *** reCAPTCHA ***
+
+var recaptcha = require('express-recaptcha');
+recaptcha.init( process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY );
+
+
 app.post('/mail', recaptcha.middleware.verify, function(req, res) {
 
-  console.log( "Received data: ");
-  console.log( req.body );
-
-  console.log( "Response from reCAPTCHA: ");
-  console.log( req.recaptcha );
+  console.log("Received data:");
+  console.log(req.body);
+  console.log("Response from reCAPTCHA:");
+  console.log(req.recaptcha);
 
   req.body.to = process.env.EMAIL;
 
   if ( req.recaptcha.error ) {
-    return res.status(403).send( { message: req.recaptcha.error.toString() } );
+    console.log("Recaptcha Error");
+    return res.status(403).send( { message: "Error From ReCAPTCHA: "+req.recaptcha.error.toString() } );
   }
   else {
     mailgun.messages().send( req.body, function sendReply (error, body) {
-      console.log( "Response from Mailgun: ");
-      console.log( body );
+      console.log("Response from Mailgun:");
+      console.log(body);
       if ( !error ) {
-        return res.status(200).send( { message: 'Your message has been sent' } );
+        console.log("Message Sent");
+        return res.status(200).send( { message: "Your message has been sent." });
       }
       else {
-        return res.status(404).send( { message: error.toString() } );
+        console.log("Cannot send message due to errors.");
+        console.log(error);
+        return res.status(404).send( { message: "Cannot send message due to server error." } );
       }
     });
   }
 });
-
-//Tell the server to serve the contents of the client folder
-app.use( express.static('client') );
 
 //Turn on the web server listening on a specific port
 app.listen( process.env.PORT );
